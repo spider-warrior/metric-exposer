206a9b3c3f24933c6be15c32f1f4d27a3232d6be
36296 <362961910@qq.com>
2024-09-27 20:25:00 +0800

    add event-loop

--- metric-exposer-common/src/main/java/cn/t/metric/common/channel/UnPooledHeapByteBuf.java
+++ metric-exposer-common/src/main/java/cn/t/metric/common/channel/UnPooledHeapByteBuf.java
@@ -1,5 +1,7 @@
 package cn.t.metric.common.channel;
 
+import cn.t.metric.common.util.HeapByteBufUtil;
+
 public class UnPooledHeapByteBuf {
     private static final int minExpandEachTimeSize = 1024;
     private static final int maxExpandEachTimeSize = 1024 * 1024;
@@ -10,10 +12,114 @@ public class UnPooledHeapByteBuf {
     private int writerIndex = 0;
     private int lastExpandSize = 0;
 
+    public UnPooledHeapByteBuf skipBytes(int length) {
+        readerIndex += length;
+        return this;
+    }
+
+    public byte readByte() {
+        return HeapByteBufUtil.getByte(buf, readerIndex++);
+    }
+
+    public boolean readBoolean() {
+        return readByte() != 0;
+    }
+
+    public short readShort() {
+        short v = HeapByteBufUtil.getShort(buf, readerIndex);
+        readerIndex += 2;
+        return v;
+    }
+
+    public int readInt() {
+        int v = HeapByteBufUtil.getInt(buf, readerIndex);
+        readerIndex += 4;
+        return v;
+    }
+
+    public long readLong() {
+        long v = HeapByteBufUtil.getLong(buf, readerIndex);
+        readerIndex += 8;
+        return v;
+    }
+
+    public void readBytes(byte[] dst, int dstIndex, int length) {
+        getBytes(readerIndex, dst, dstIndex, length);
+        readerIndex += length;
+    }
+
+    public void readBytes(byte[] dst) {
+        readBytes(dst, 0, dst.length);
+    }
+
+    public void getBytes(int index, byte[] dst, int dstIndex, int length) {
+        System.arraycopy(buf, index, dst, dstIndex, length);
+    }
+
+    public char readChar() {
+        return (char) readShort();
+    }
+
+    public float readFloat() {
+        return Float.intBitsToFloat(readInt());
+    }
+
+    public double readDouble() {
+        return Double.longBitsToDouble(readLong());
+    }
+
+    public UnPooledHeapByteBuf writeBoolean(boolean value) {
+        return writeByte(value ? (byte)1 : (byte)0);
+    }
+
     public UnPooledHeapByteBuf writeByte(byte value) {
         ensureWritable(1);
         buf[writerIndex++] = value;
         return this;
+    }
+
+    public UnPooledHeapByteBuf writeShort(int value) {
+        ensureWritable(2);
+        HeapByteBufUtil.setShort(buf, writerIndex, value);
+        writerIndex += 2;
+        return this;
+    }
+
+    public UnPooledHeapByteBuf writeInt(int value) {
+        ensureWritable(4);
+        HeapByteBufUtil.setInt(buf, writerIndex, value);
+        writerIndex += 4;
+        return this;
+    }
+
+    public UnPooledHeapByteBuf writeLong(long value) {
+        ensureWritable(8);
+        HeapByteBufUtil.setLong(buf, writerIndex, value);
+        writerIndex += 8;
+        return this;
+    }
+
+    public UnPooledHeapByteBuf writeChar(int value) {
+        return writeShort(value);
+    }
+
+    public UnPooledHeapByteBuf writeFloat(float value) {
+        return writeInt(Float.floatToRawIntBits(value));
+    }
+
+    public UnPooledHeapByteBuf writeDouble(double value) {
+        return writeLong(Double.doubleToRawLongBits(value));
+    }
+
+    public UnPooledHeapByteBuf writeBytes(byte[] src, int srcIndex, int length) {
+        ensureWritable(length);
+        System.arraycopy(src, srcIndex, buf, writerIndex, length);
+        writerIndex += length;
+        return this;
+    }
+
+    public UnPooledHeapByteBuf writeBytes(byte[] src) {
+        return writeBytes(src, 0, src.length);
     }
 
     public int writerIndex() {
@@ -22,6 +128,10 @@ public class UnPooledHeapByteBuf {
 
     public int readerIndex() {
         return readerIndex;
+    }
+
+    public int readableBytes() {
+        return writerIndex - readerIndex;
     }
 
     final void ensureWritable(int writeBytes) {
