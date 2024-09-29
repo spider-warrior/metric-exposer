a773d34db37628792a5775e8e72a51e5c7fcdf2a
36296 <362961910@qq.com>
2024-09-29 15:46:55 +0800

    add event-loop

--- metric-exposer-server/src/main/java/cn/t/metric/server/MetricExposerServerApplication.java
+++ metric-exposer-server/src/main/java/cn/t/metric/server/MetricExposerServerApplication.java
@@ -6,6 +6,7 @@ import cn.t.metric.common.channel.ChannelInitializer;
 import cn.t.metric.common.channel.SingleThreadEventLoop;
 import cn.t.metric.common.channel.UnPooledHeapByteBuf;
 import cn.t.metric.common.handler.ChannelHandler;
+import cn.t.metric.common.util.ExceptionUtil;
 
 import java.nio.channels.SocketChannel;
 
@@ -21,6 +22,9 @@ public class MetricExposerServerApplication {
 
             @Override
             public void read(ChannelContext ctx, UnPooledHeapByteBuf byteBuf) {
+//                if(true) {
+//                    throw new RuntimeException("on purpose");
+//                }
                 byte[] array = new byte[byteBuf.readableBytes()];
                 byteBuf.readBytes(array);
                 System.out.println("[channel-read]read message: " + new String(array));
@@ -38,7 +42,7 @@ public class MetricExposerServerApplication {
 
             @Override
             public void error(ChannelContext ctx, Throwable t) {
-                System.out.println("error: " + t);
+                System.out.println("[channel-error]: " + ExceptionUtil.getErrorMessage(t));
             }
         });
         ServerBootstrap.bind(bindAddress, bindPrt, subChannelInitializer, new SingleThreadEventLoop(), new SingleThreadEventLoop());
