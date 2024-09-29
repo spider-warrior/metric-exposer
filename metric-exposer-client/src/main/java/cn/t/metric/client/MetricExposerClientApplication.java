a0f496fd31bfbcad4e4ea114f38a0d40df993196
36296 <362961910@qq.com>
2024-09-29 18:52:42 +0800

    add event-loop

--- metric-exposer-client/src/main/java/cn/t/metric/client/MetricExposerClientApplication.java
+++ metric-exposer-client/src/main/java/cn/t/metric/client/MetricExposerClientApplication.java
@@ -1,14 +1,13 @@
 package cn.t.metric.client;
 
+import cn.t.metric.client.channel.SocketChannelInitializer;
 import cn.t.metric.common.bootstrap.ClientBootstrap;
-import cn.t.metric.common.channel.ChannelInitializer;
 import cn.t.metric.common.channel.SingleThreadEventLoop;
 
 public class MetricExposerClientApplication {
     public static void main(String[] args) throws Exception {
         String serverHost = "127.0.0.1";
         int serverPort = 5000;
-        ChannelInitializer channelInitializer = (ctx, ch) -> ctx.getPipeline().addChannelHandlerLast(new CmdRequestHandler());
-        ClientBootstrap.connect(serverHost, serverPort, channelInitializer, new SingleThreadEventLoop());
+        ClientBootstrap.connect(serverHost, serverPort, new SocketChannelInitializer(), new SingleThreadEventLoop());
     }
 }
---
+++ metric-exposer-client/src/main/java/cn/t/metric/client/channel/SocketChannelInitializer.java
@@ -0,0 +1,14 @@
+package cn.t.metric.client.channel;
+
+import cn.t.metric.client.CmdRequestHandler;
+import cn.t.metric.common.channel.ChannelContext;
+import cn.t.metric.common.channel.ChannelInitializer;
+
+import java.nio.channels.Channel;
+
+public class SocketChannelInitializer implements ChannelInitializer {
+    @Override
+    public void initChannel(ChannelContext ctx, Channel ch) throws Exception {
+        ctx.getPipeline().addChannelHandlerLast(new CmdRequestHandler());
+    }
+}
